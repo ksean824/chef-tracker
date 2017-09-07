ee1d13d1f54cabc2b36a1a5311ed872898583be1
Kevin Shaughnessy <kevin.shaughnessy.wnhx@statefarm.com>
2017-09-07 12:34:21 -0700

    part 2: routes

--- app/models/chef.js
+++ app/models/chef.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr('string'),
     isCookingToday: DS.attr('boolean'),
-    numberOfStudents: DS.attr('number')
+    numberOfStudents: DS.attr('number'),
+    description: DS.attr('string')
 });
--- app/router.js
+++ app/router.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 import config from './config/environment';
 
+
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
 Router.map(function() {
+  this.route('chefs', function() {
+    this.route('chef', {path: ':chef_id'})
+  })
+  this.route('about')
+  this.route('training')
+  this.route('favorite-word', {path: 'favorite-word/:word'})
 });
 
 export default Router;
